2c431b25b4c70088cab387592e0225c9303e6d1c
samaelmarjan <samaaelmarjan@gmail.com>
2024-05-25 13:19:04 +0600

    middlewares

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config();
 
 const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
 // port
 const port = 5000;
